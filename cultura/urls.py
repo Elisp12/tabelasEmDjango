edd75f85b9e496859a049383d5358b15c12dae43
Elisp12 <eliseuspdam.1999@gmail.com>
2024-08-28 03:01:47 -0300

    update -> se o mesmo dado entre duas tabelas sao iguais entao atualiza  a coluna referida

--- cultura/urls.py
+++ cultura/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from cultura.views import home
+from cultura.views import *
 
 urlpatterns = [
     path('', home, name='home'),
+    path('update/',update, name='upsdate'),
 ]
--- cultura/views.py
+++ cultura/views.py
@@ -18,3 +18,29 @@ def home(request):
     context = {    
     }
     return render(request, 'index.html', context = context)
+
+def update(request):
+     
+    lista_culturas = Tb_culturas.objects.all().values('nome', 'tipo') # lista tabela de culturas
+    
+    lista_armazem = Tb_armazem.objects.all() # lista tabela de armazem
+    
+    # não consigo salvar o proximo dado sem que o anterior a este não seja modificado   (histórico)
+    # for armazem in lista_armazem:
+    #     armazem.old_nome = armazem.nome
+    #     armazem.old_tipo = armazem.tipo
+    #     armazem.save()
+
+    for cultura in lista_culturas:
+        # tenta buscar um objeto da tb_armazem com o mesmo nome 
+        dados_armazem = Tb_armazem.objects.filter(nome =cultura['nome']).first()
+
+        if dados_armazem: # se existe objeto com o mesmo nome  
+
+            dados_armazem.tipo = cultura['tipo'] # atualiza o tipo
+            dados_armazem.save() # salva na tabela
+
+
+    context = {    
+    }
+    return redirect('home')
