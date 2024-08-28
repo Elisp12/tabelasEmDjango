c2ce690e6daf9819e24be44ae7f19478bc2e535a
Elisp12 <eliseuspdam.1999@gmail.com>
2024-08-27 23:02:44 -0300

    migrações

--- cultura/models.py
+++ cultura/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Tb_culturas(models.MOdel):
+class Tb_culturas(models.Model):
     index = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=50)
     tipo = models.CharField(max_length=100)
